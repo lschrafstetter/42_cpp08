0762090dc9d1b8755dd979018ead84313fb505ee
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-21 15:38:54 +0200

    Added header protection...

--- ex01/src/Span.cpp
+++ ex01/src/Span.cpp
@@ -1,25 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/21 15:13:50 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/21 15:38:09 by lschrafs         ###   ########.fr       */
+/*   Created: 2022/10/21 15:37:40 by lschrafs          #+#    #+#             */
+/*   Updated: 2022/10/21 15:37:41 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASYFIND_H
-#define EASYFIND_H
-
-#include <iterator>
-#include <algorithm>
-
-template <typename T>
-typename T::iterator easyfind(T &container, const int &target) {
-  typename T::iterator it = std::find(container.begin(), container.end(), target);
-  return it;
-}
-
-#endif
--- ex01/src/Span.hpp
+++ ex01/src/Span.hpp
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/21 15:13:50 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/21 15:38:09 by lschrafs         ###   ########.fr       */
+/*   Created: 2022/10/21 15:37:45 by lschrafs          #+#    #+#             */
+/*   Updated: 2022/10/21 15:38:34 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASYFIND_H
-#define EASYFIND_H
 
-#include <iterator>
-#include <algorithm>
-
-template <typename T>
-typename T::iterator easyfind(T &container, const int &target) {
-  typename T::iterator it = std::find(container.begin(), container.end(), target);
-  return it;
-}
-
-#endif
