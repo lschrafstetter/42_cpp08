422c76011b37940a090b5bf4ca6ba07d5c902c93
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-21 15:34:07 +0200

    finished ex00

--- ex00/src/easyfind.hpp
+++ ex00/src/easyfind.hpp
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/12 15:08:40 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/21 15:33:05 by lschrafs         ###   ########.fr       */
+/*   Created: 2022/10/21 15:13:50 by lschrafs          #+#    #+#             */
+/*   Updated: 2022/10/21 15:29:48 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "easyfind.hpp"
-#include <iostream>
-#include <vector>
-#include <list>
+#include <iterator>
+#include <algorithm>
 
-int main(void) {
-  std::vector<int> v = { 1, 0, 2, 0, 3, 0, 4 };
-  std::cout << *easyfind(v, 3) << std::endl;
-  std::cout << *(easyfind(v, 0)++) << std::endl << std::endl;
-
-  std::list<int> l = { 1, 0, 2, 0, 3, 0, 4 };
-  std::cout << *easyfind(l, 3) << std::endl;
-  std::cout << *(easyfind(l, 0)++) << std::endl;
+template <typename T>
+typename T::iterator easyfind(T &container, const int &target) {
+  typename T::iterator it = std::find(container.begin(), container.end(), target);
+  return it;
 }
