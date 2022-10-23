4cd81155c0aa7d2c7286b99b06a188d6a3db4062
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-23 18:18:20 +0200

    finished ex02 +  tests

--- ex02/src/MutantStack.hpp
+++ ex02/src/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 12:10:01 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/23 16:51:08 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/10/23 18:07:08 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 #define MUTANT_STACK_H
 
 #include <stack>
+
+#define DEFAULT "\033[39m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define BLACK "\033[30m"
+#define GREY "\033[90m"
+#define CYAN "\033[36m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[94m"
+#define MAGENTA "\033[35m"
 
 template <typename T>
 class MutantStack : public std::stack<T> {
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:08:40 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/23 16:47:03 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/10/23 18:17:29 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,44 @@
 #include <string>
 #include <list>
 
-int main(void) {
+int main(void) {  
+  std::cout << YELLOW << "----- original stack -----" << std::endl;
+
+  std::stack<int> stack;
   
+  std::cout << "*pushing and popping some stuff*" << std::endl;
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  stack.pop();
+  stack.push(5);
+  std::cout << "Emptying stack from top to bottom:" << std::endl;
+  while (!stack.empty()) {
+    std::cout << stack.top() << std::endl;
+    stack.pop();
+  }
+
+  std::cout << GREEN << "----- mutant stack -----" << std::endl;
+  
+  MutantStack<int> mutant_stack;
+
+  std::cout << "*pushing and popping some stuff*" << std::endl;
+  mutant_stack.push(1);
+  mutant_stack.push(2);
+  mutant_stack.push(3);
+  mutant_stack.push(4);
+  mutant_stack.pop();
+  mutant_stack.push(5);
+  std::cout << "Now iterable, much wow!" << std::endl;
+  for (MutantStack<int>::iterator it = mutant_stack.begin(); it != mutant_stack.end(); it++) {
+    std::cout << *it << std::endl;
+  }
+  std::cout << "Printed everything and it is not empty. Amazing!" << std::endl;
+  std::cout << "Emptying stack from top to bottom:" << std::endl;
+  while (!mutant_stack.empty()) {
+    std::cout << mutant_stack.top() << std::endl;
+    mutant_stack.pop();
+  }
   return 0;
 }
