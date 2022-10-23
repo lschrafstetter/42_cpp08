3c2163bb258cc158037acec525769aaf95d69e85
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-23 16:53:28 +0200

    format

--- ex01/src/Span.hpp
+++ ex01/src/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 15:37:45 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/22 11:56:24 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/10/22 17:33:34 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@
 #include <iterator>
 
 #define DEFAULT "\033[39m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
 #define BLACK "\033[30m"
 #define GREY "\033[90m"
-#define RED "\033[31m"
 #define CYAN "\033[36m"
-#define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 #define BLUE "\033[94m"
 #define MAGENTA "\033[35m"
