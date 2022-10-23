66e9a80998ecec6a3e68df067b6339abd13dc5f1
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-23 20:03:05 +0200

    fixed tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:08:40 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/21 15:33:05 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/10/23 20:00:14 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,25 @@
 #include <list>
 
 int main(void) {
-  std::vector<int> v = { 1, 0, 2, 0, 3, 0, 4 };
+  std::vector<int> v;
+  v.push_back(1);
+  v.push_back(0);
+  v.push_back(2);
+  v.push_back(0);
+  v.push_back(3);
+  v.push_back(0);
+  v.push_back(4);
   std::cout << *easyfind(v, 3) << std::endl;
-  std::cout << *(easyfind(v, 0)++) << std::endl << std::endl;
+  std::cout << *(++easyfind(v, 0)) << std::endl << std::endl;
 
-  std::list<int> l = { 1, 0, 2, 0, 3, 0, 4 };
+  std::list<int> l;
+  l.push_back(1);
+  l.push_back(0);
+  l.push_back(2);
+  l.push_back(0);
+  l.push_back(3);
+  l.push_back(0);
+  l.push_back(4);
   std::cout << *easyfind(l, 3) << std::endl;
-  std::cout << *(easyfind(l, 0)++) << std::endl;
+  std::cout << *(++easyfind(l, 0)) << std::endl;
 }
